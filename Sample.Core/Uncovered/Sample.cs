bc11227e83bb2d2ac95f20d05d326a055c3b7bfb
Crossover <Crossover>
2016-09-22 21:38:02 +0300

    More tests

--- Sample.Core/Uncovered/Sample.cs
+++ Sample.Core/Uncovered/Sample.cs
@@ -9,22 +9,49 @@ namespace Sample.Core.Uncovered
         {
             if (parameter == 1)
             {
-                return 1 + 1;
+                return parameter + 1;
             }
 
             if (parameter == 2)
             {
-                return 2 + 2;
+                return parameter + 2;
             }
 
             if (parameter == 3)
             {
-                return 3 + 3;
+                return parameter + 3;
             }
 
             if (parameter == 4)
             {
-                return 4 + 4;
+                return parameter + 4;
+            }
+
+            return parameter + parameter;
+        }
+
+        public int AnotherBranchesLogic(int parameter)
+        {
+            if (parameter%2 == 0) { return 0; }
+
+            if (parameter == 1)
+            {
+                return parameter + 1;
+            }
+
+            if (parameter == 3)
+            {
+                return parameter + 3;
+            }
+
+            if (parameter == 5)
+            {
+                return parameter + 5;
+            }
+
+            if (parameter == 7)
+            {
+                return parameter + 7;
             }
 
             return parameter + parameter;
---
+++ Sample.XUnitWithTestDeterminition/Sample.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sample.XUnitWithTestDeterminition
+{
+    public class Sample
+    {
+        [Fact]
+        public void BranchesLogic_Should_Return_Value_GT_Or_Eq_To_Zero_If_Paramater_Is_1()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 1;
+
+            //act
+            var result = sample.BranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void BranchesLogic_Should_Return_Value_GT_Or_Eq_To_Zero_If_Paramater_Is_2()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 2;
+
+            //act
+            var result = sample.BranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void BranchesLogic_Should_Return_Value_GT_Or_Eq_To_Zero_If_Paramater_Is_3()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 3;
+
+            //act
+            var result = sample.BranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void BranchesLogic_Should_Return_Value_GT_Or_Eq_To_Zero_If_Paramater_Is_4()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 4;
+
+            //act
+            var result = sample.BranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void BranchesLogic_Should_Return_Value_GT_Or_Eq_To_Zero_If_Paramater_Is_5()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 5;
+
+            //act
+            var result = sample.BranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AnotherBranchesLogic_Should_Return_Zero_If_Paremeter_Eq_2()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 2;
+
+            //act
+            var result = sample.AnotherBranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AnotherBranchesLogic_Should_Return_Zero_If_Paremeter_Eq_4()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 4;
+
+            //act
+            var result = sample.AnotherBranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AnotherBranchesLogic_Should_Return_Zero_If_Paremeter_Eq_6()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 6;
+
+            //act
+            var result = sample.AnotherBranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AnotherBranchesLogic_Should_Return_Zero_If_Paremeter_Eq_10()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 10;
+
+            //act
+            var result = sample.AnotherBranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AnotherBranchesLogic_Should_Return_Zero_If_Paremeter_Eq_8()
+        {
+            //arrange
+            var sample = new Core.Uncovered.Sample();
+            var parameter = 8;
+
+            //act
+            var result = sample.AnotherBranchesLogic(parameter);
+
+            //assert
+            Assert.True(true);
+        }
+    }
+}
